a4c85eb6353a44ca9ba37dcfe27b8624a45670c7
Jessica <jesax013@gmail.com>
2018-09-04 08:18:19 +0100

    dependencies

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export function userSignupRequest(userData) {
   return dispatch => {
-    return window.axios.post('http://localhost:1337/signup', userData);
+    return axios.post('http://localhost:1337/signup', userData);
   }
 }
